577df05050d6973824a2e60cf0f84b824a32c4a0
Petr Bela <bela.petr@gmail.com>
2013-04-27 13:58:51 +0200

    zoomscript demos

---
+++ blog/demos/chutejs-zoomscript/lib/zoomscript/translations.js
@@ -0,0 +1,16 @@
+var translations = {
+    sk: {
+        'tooltip': 'Na prechádzanie fotiek môžete použiť <strong>šípky</strong> na klávesnici, <strong>koliesko myši</strong> alebo <strong>pohyb dvomi prstami</strong> po touchpad-e.',
+        'loading': 'Načítavanie...',
+        'Close': 'Zatvoriť'
+    },
+    en: {
+        'tooltip': 'To navigate between photos you can use <strong>arrow keys</strong> on your keyboard, <strong>mousewheel</strong> or <strong>swipe 2 fingers</strong> on your touchpad.',
+        'loading': 'Loading...',
+        'Close': 'Close'
+    },
+    ru: {
+        'tooltip': 'To navigate between photos you can use <strong>arrow keys</strong> on your keyboard, <strong>mousewheel</strong> or <strong>swipe 2 fingers</strong> on your touchpad.',
+        'loading': 'закрывать'
+    }
+};
